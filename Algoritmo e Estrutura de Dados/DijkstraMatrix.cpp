f0236313fca8bf940287e9d911f13253add1170d
Fábio Carrião Filho <fabio.carriao.f@gmail.com>
2023-01-04 10:00:28 -0300

    Translated variables to english

--- Algoritmo e Estrutura de Dados/DijkstraMatrix.cpp
+++ Algoritmo e Estrutura de Dados/DijkstraMatrix.cpp
@@ -6,46 +6,45 @@
 
 using namespace std;
 
-int custo[GRAPH_SIZE];
-int anterior[GRAPH_SIZE];
-bool fechado[GRAPH_SIZE];
+int cost[GRAPH_SIZE];
+int previous[GRAPH_SIZE];
+bool closed[GRAPH_SIZE];
 
-int grafo[GRAPH_SIZE][GRAPH_SIZE] = {
-    {0,       2,    INF,    1,   INF,    INF, INF},
-    {INF,     0,    INF,    3,    10,    INF, INF},
-    {4,     INF,      0,  INF,   INF,      5, INF},
-    {INF,   INF,      2,    0,     3,      8,   4},
-    {INF,   INF,    INF,  INF,     0,    INF,   6},
-    {INF,   INF,    INF,  INF,   INF,      0, INF},
-    {INF,   INF,    INF,  INF,   INF,      1,   0},
+int graph[GRAPH_SIZE][GRAPH_SIZE] = {
+{0, 2, INF, 1, INF, INF, INF},
+{INF, 0, INF, 3, 10, INF, INF},
+{4, INF, 0, INF, INF, 5, INF},
+{INF, INF, 2, 0, 3, 8, 4},
+{INF, INF, INF, INF, 0, INF, 6},
+{INF, INF, INF, INF, INF, 0, INF},
+{INF, INF, INF, INF, INF, 1, 0},
 };
 
-void dijkstra(int origem){
+void dijkstra(int source){
     int vcm = -1;
-
     for(int i = 0; i < GRAPH_SIZE; i++){
-        custo[i] = INF;
-        anterior[i] = -1;
-        fechado[i] = false;
+        cost[i] = INF;
+        previous[i] = -1;
+        closed[i] = false;
     }
 
-    custo[origem] = 0;
+    cost[source] = 0;
 
     while(true){
         vcm = -1;
         for(int i = 0; i < GRAPH_SIZE; i++){
-            if(!fechado[i] && (vcm == -1 || custo[i] < custo[vcm])){
+            if(!closed[i] && (vcm == -1 || cost[i] < cost[vcm])){
                 vcm = i;
             }
         }
 
         if (vcm < 0) break;
-        fechado[vcm] = true;
+        closed[vcm] = true;
 
         for(int i = 0; i < GRAPH_SIZE; i++){
-            if((grafo[vcm][i] != 0) && (custo[i] > custo[vcm] + grafo[vcm][i])){
-                custo[i] = custo[vcm] + grafo[vcm][i];
-                anterior[i] = vcm;
+            if((graph[vcm][i] != 0) && (cost[i] > cost[vcm] + graph[vcm][i])){
+                cost[i] = cost[vcm] + graph[vcm][i];
+                previous[i] = vcm;
             }
         }
     }
@@ -55,7 +54,7 @@ int main(){
     
     dijkstra(0);
     for(int i = 0; i < GRAPH_SIZE; i++){
-        cout << "Custo de 0 a " << i << ": " << custo[i] << endl;
+        cout << "Cost from 0 to " << i << ": " << cost[i] << endl;
     }
 
     return 0;
